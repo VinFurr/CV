9dc0633aebfbef61f33a0e2ede249991e8ce8066
Vincent <49636101+VinFurr@users.noreply.github.com>
2024-05-25 15:11:20 +0200

    add animation

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll('.descrAnim');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
